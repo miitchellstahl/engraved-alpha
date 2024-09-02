cafeb3a27e5f06a6a5909bae6290c7bff6636818
Mitchell Stahl <mitchstahl11@gmail.com>
2024-09-02 01:27:50 -0400

    Onboarding Nav Menu V1

---
+++ frontend/src/forms/Obituary Form/OnboardingNavigationMenu.tsx
@@ -0,0 +1,51 @@
+import { BookOpen, Link } from "lucide-react";
+import AddToProfileNavigationButton from "@/components/AddToProfileNavigationButton";
+import { Separator } from "@/components/ui/separator";
+import InProgressNavigationButton from "@/components/InProgressNavigationButton";
+
+type Props = {
+  appendEulogy: (item: any) => void;
+  initialData: any;
+  eulogyFields: any;
+};
+
+const OnboardingNavigationMenu = ({
+  appendEulogy,
+  eulogyFields,
+  initialData,
+}: Props) => {
+  return (
+    <div className="space-y-4">
+      <div className="flex flex-col gap-2">
+        <AddToProfileNavigationButton
+          buttonText="Add eulogy"
+          handleAppend={appendEulogy}
+          IconComponent={BookOpen}
+        />
+        <AddToProfileNavigationButton
+          buttonText="Add funeral link"
+          handleAppend={appendEulogy}
+          IconComponent={Link}
+        />
+        <Separator />
+        <span>Obituary:</span>
+        {initialData?.obituary && (
+          <InProgressNavigationButton
+            imageUrl={initialData?.profilePhotoUrl}
+            buttonText={`${initialData?.firstName}'s Obituary`}
+          />
+        )}
+        <span>Eulogies:</span>
+        {[...eulogyFields].reverse().map((fields, index) => (
+          <InProgressNavigationButton
+            IconComponent={BookOpen}
+            key={index}
+            buttonText={`Eulogy ${index}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingNavigationMenu;
